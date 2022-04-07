935a3352d646138eece6ca5394b18a46efaf33f2
Benjamin Matthews <72388997+leodanben@users.noreply.github.com>
2022-04-08 01:44:18 +0300

    Added flutter to install list

--- install-with-chocolatey.ps1
+++ install-with-chocolatey.ps1
@@ -5,4 +5,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install External Apps Using Chocolatey
-choco install php vscode jetbrainstoolbox phpstorm googlechrome microsoft-office-deployment microsoft-teams whatsapp androidstudio python visualstudio2022community docker-desktop drawio inkscape gimp mysql.workbench obs-studio screamingfrog wsl2 wsl-opensuse wsl-ubuntu-2004 virtualbox virtualbox-guest-additions-guest vagrant spotify twitch -y
+choco install php vscode jetbrainstoolbox phpstorm googlechrome microsoft-office-deployment microsoft-teams whatsapp androidstudio python visualstudio2022community docker-desktop drawio inkscape gimp mysql.workbench obs-studio screamingfrog wsl2 wsl-opensuse wsl-ubuntu-2004 virtualbox virtualbox-guest-additions-guest vagrant spotify twitch flutter -y
